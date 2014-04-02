dc0bd9dc71e2a06ea7cfbc6e123df8e652b8860b
Joseph Bentley <seppybentley@gmail.com>
2014-04-02 12:12:37 +0100

    Added simple functionality

---
+++ biddyquestiiv/Assets/Scripts/MoverScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoverScript : MonoBehaviour {
+
+	bool _movingUp = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(_movingUp)
+		{
+			this.rigidbody2D.velocity = new Vector2(0,1);
+		}
+		else
+		{
+			this.rigidbody2D.velocity = new Vector2(0,-1);
+		}
+
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			if(_movingUp)
+			{
+				_movingUp = false;
+			}
+			else
+			{
+				_movingUp = true;
+			}
+		}
+	}
+}
